72487bd545cdcd7b309a457458889b28f58d8846
olekmali <olekmali@ieee.org>
2016-11-25 21:35:24 -0600

    MASJOR correction in TeleOp -- corrected a serious arm control error that was introduced in Matthew's Laptop code after the first competition code was already uploaded -- would affect subsequent flushes of the code!
     Crearted MattSetupSensors and moved sensor initialization there from PushBotAuto
    
     Created MattAutomation class container. Not started code migration there though

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MattAutomation.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by olekmali on 11/25/2016.
+ */
+
+public class MattAutomation {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MattSetupSensors.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsDigitalTouchSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+
+/**
+ * Created on 11/25/2016.
+ */
+
+public class MattSetupSensors {
+
+    /* Public sensor members. */
+    /* Touch Sensor */
+    ModernRoboticsDigitalTouchSensor touchSensorFront = null;
+    ModernRoboticsDigitalTouchSensor touchSensorArm = null;
+
+    //ODS *Addon
+    OpticalDistanceSensor lightSensor;
+
+    //MRColor Sensor *Addon
+    ColorSensor colorSensor;
+
+
+
+    /* local OpMode members. */
+    HardwareMap hardwareMap =  null;
+
+
+    /* Constructor */
+    public MattSetupSensors() {
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hardwareMap = ahwMap;
+
+        //Define touchSensorFront
+        touchSensorFront = (ModernRoboticsDigitalTouchSensor) hardwareMap.touchSensor.get("touchSensorFront");
+        touchSensorArm = (ModernRoboticsDigitalTouchSensor) hardwareMap.touchSensor.get("touchSensorArm");
+
+        //Define lightSensor & enbale led
+        lightSensor = hardwareMap.opticalDistanceSensor.get("lightSensor");
+        lightSensor.enableLed(true);
+
+    }
+
+
+}
