fcdc5af4c93558f230cf8bc5779cc1b171020a90
olekmali <olekmali@ieee.org>
2016-11-25 23:35:08 -0600

    MASJOR correction in TeleOp -- corrected a serious arm control error that was introduced in Matthew's Laptop code after the first competition code was already uploaded -- would affect subsequent flushes of the code!
     Crearted MattSetupSensors and moved sensor initialization there from PushBotAuto
    
     Created MattAutomation class container. Common code is implemented here with timouts but not yet with max drive distance "timeouts"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MattSetupPushbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MattSetupPushbot.java
@@ -30,13 +30,11 @@ public class MattSetupPushbot
     public DcMotor  armMotor    = null;
 
 
-    public static final double ARM_UP_POWER    =  0.45 ;
-    public static final double ARM_DOWN_POWER  = -0.45 ;
-
-    public static final double COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
-    public static final double DRIVE_GEAR_REDUCTION    = 2 ;     // This is < 1.0 if geared UP
-    public static final double WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
-    public static final double COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+    public static final double COUNTS_PER_MOTOR_REV     = 1440 ;    // eg: TETRIX Motor Encoder
+    public static final double DRIVE_GEAR_REDUCTION     = 2.0 ;     // This is < 1.0 if geared UP
+    public static final double WHEEL_DIAMETER_INCHES    = 4.0 ;     // For figuring circumference
+    public static final double COUNTS_PER_INCH          = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+    public static final double INCHES_PER_ANGLE         = (6 / 90);
 
     /* local members. */
     HardwareMap hwMap           =  null;
