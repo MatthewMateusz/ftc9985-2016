59030f11ab1419aaa9c37fccd3a9c23d048f1062
Matthew Mateusz <mateuszm@matthewm.com.pl>
2016-12-11 13:36:15 -0600

    Changed to gear ratio.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MattSetupActuators.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MattSetupActuators.java
@@ -30,7 +30,7 @@ public class MattSetupActuators
 
 
     public static final double COUNTS_PER_MOTOR_REV     = 1440;    // eg: TETRIX Motor Encoder
-    public static final double DRIVE_GEAR_REDUCTION     = 2.0;     // This is < 1.0 if geared UP
+    public static final double DRIVE_GEAR_REDUCTION     = 1.0;     // This is < 1.0 if geared UP
     public static final double WHEEL_DIAMETER_INCHES    = 4.0 * (24.5/24.0);  // wheel circumference with real-life travel distance adjustment
     public static final double WHEEL_SEPARATION_INCHES  = 14.0;
     public static final double COUNTS_PER_INCH          = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.14159269);
